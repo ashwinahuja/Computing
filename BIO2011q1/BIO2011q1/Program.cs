1a4cce4182004e931eed2c82dfa61b596702293e
Ashwin Ahuja <ashwin.ahuja@gmail.com>
2015-11-13 02:24:50 +0000

    Added BIO 2012

---
+++ BIO2011q1/BIO2011q1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace BIO2011q1
+{
+	class MainClass
+	{
+		public static char numberOne = 'A';
+		public static char numberTwo = 'A';
+		public static char nextNumber = 'A';
+		static void findNext()
+		{
+			int number1 = ConvertToNumber (numberOne);
+			int number2 = ConvertToNumber (numberTwo);
+			int temp = number1 + number2;
+			char replacement = ConvertToCharacter (temp);
+			numberOne = numberTwo;
+			numberTwo = replacement;
+		}
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Enter Input");
+			string togetherI = Console.ReadLine ();
+			string[] input = togetherI.Split(' ');
+			numberOne = Convert.ToChar(input[0]);
+			numberTwo = Convert.ToChar (input [1]);
+			int counter = 2;
+			int targetNumber = Convert.ToInt32 (input [2]);
+			if (targetNumber == 1) {
+				numberTwo = numberOne;
+			}
+			else
+			{
+				while (counter != targetNumber) {
+				findNext ();
+				counter++;
+				}
+			}
+			Console.WriteLine (numberTwo);
+			Console.ReadLine ();
+		}
+		static int ConvertToNumber(char input)
+		{
+			string definition = "0ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			for (int i = 0; i < definition.Length; i++) {
+				if (input == definition [i]) {
+					return i;
+				}
+			}
+			return 0;
+		}
+		static char ConvertToCharacter(int temp)
+		{
+			string definition = "0ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			if (temp > 26) {
+				temp = temp - 26;
+			}
+			return definition [temp];
+		}
+	}
+}
---
+++ Upside-Down/Upside-Down/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace UpsideDown
+{
+	class MainClass
+	{
+		public static double findNextUpsideDown(double lastUpsideDown)
+		{
+			bool gotOne = false;
+			int counter = Convert.ToInt32 (lastUpsideDown);
+			while (!gotOne) {
+				counter++;
+				gotOne = testCounter (counter);
+			}
+			return Convert.ToDouble (counter);
+		}
+		public static bool testCounter(int test)
+		{
+			string forUse = Convert.ToString (test);
+			int length = forUse.Length;
+			for (int i = 0; i < (length + 1) / 2; i++) {
+				int test1 = int.Parse (forUse [i].ToString ());
+				int temp = i + 1;
+				temp = length - temp;
+				int test2 = int.Parse (forUse [temp].ToString ());
+				if (test1 + test2 != 10)
+					return false;
+			}
+			return true;
+		}
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Enter Input");
+			double lastUpsideDown = 0;
+			int input = Convert.ToInt32 (Console.ReadLine ());
+			for (int i = 0; i < input; i++) {
+				lastUpsideDown = findNextUpsideDown (lastUpsideDown);
+			}
+			Console.WriteLine (lastUpsideDown);
+			Console.ReadLine ();
+		}
+	}
+}
